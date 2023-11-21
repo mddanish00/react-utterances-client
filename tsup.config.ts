01edc56208d7ffc4732cd2b75e8f852d67c41ce2
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2023-11-21 21:17:11 +0800

    build: using tsup instead of vite build
    Ironically better and simpler

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+	entry: ['src/index.ts'],
+	format: ['esm', 'cjs'],
+	clean: true,
+	dts: true,
+	minify: true,
+	banner: () => {
+		return { js: '"use client";' };
+	},
+    injectStyle: true
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	build: {
-		lib: {
-			entry: resolve(__dirname, 'src/index.ts'),
-			formats: ['es', 'cjs'],
-			fileName: 'index',
-		},
-		rollupOptions: {
-			external: ['react', 'react/jsx-runtime'],
-			output: {
-				intro: "import './style.css'",
-				banner: "'use client';",
-				interop: 'auto',
-				globals: { react: 'React' },
-			},
-		},
-	},
-	plugins: [react(), dts({ rollupTypes: true })],
+	// build: {
+	// 	lib: {
+	// 		entry: resolve(__dirname, 'src/index.ts'),
+	// 		formats: ['es', 'cjs'],
+	// 		fileName: 'index',
+	// 	},
+	// 	rollupOptions: {
+	// 		external: ['react', 'react/jsx-runtime'],
+	// 		output: {
+	// 			intro: "import './style.css'",
+	// 			banner: "'use client';",
+	// 			interop: 'auto',
+	// 			globals: { react: 'React' },
+	// 		},
+	// 	},
+	// },
+	// plugins: [react(), dts({ rollupTypes: true })],
+	plugins: [react()],
 });
