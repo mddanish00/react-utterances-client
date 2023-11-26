dda241fd7218310b41db397cdcf7f416bb0bb23c
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2023-11-26 17:34:08 +0800

    chore: update demo and demo icon

--- demo/App.tsx
+++ demo/App.tsx
@@ -31,18 +31,39 @@ const App = () => {
 	};
 
 	return (
-		<div>
-			<select value={selectedTheme} onChange={handleChange}>
-				{themeList.map((t, index) => (
-					<option key={t + index} value={t}>
-						{t}
-					</option>
-				))}
-			</select>
+		<div
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				flexDirection: 'column',
+			}}
+		>
+			<h1>react-utterances-client🔮 Demo</h1>
+			<p style={{ fontSize: '12px' }}>
+				This icon svg that used as the demo icon, 🔮 is a part of googlefonts/noto-emoji project
+				that licensed under the Apache License 2.0.
+			</p>
+			<h2>Comments</h2>
+			<div
+				style={{
+					marginTop: '2rem',
+					marginBottom: '2rem',
+				}}
+			>
+				<p style={{ display: 'inline-block' }}>Theme:&nbsp;</p>
+				<select value={selectedTheme} onChange={handleChange}>
+					{themeList.map((t, index) => (
+						<option key={t + index} value={t}>
+							{t}
+						</option>
+					))}
+				</select>
+			</div>
 			<Utterances
 				repo="mddanish00/mddanish00-devblog-comments"
 				theme={selectedTheme}
-				issueTerm="url"
+				issueTerm="title"
 			/>
 		</div>
 	);
